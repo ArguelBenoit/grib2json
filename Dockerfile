c270b2249a56b7a908d614090e5b01be04ff881d
user <benoit.arguel@gmail.com>
2018-07-06 11:25:52 +0200

    update v2 api

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,10 @@ WORKDIR /usr/grib2json
 
 RUN apt-get update -y
 RUN apt-get install maven -y
+RUN apt-get install curl
 RUN mvn package
 RUN mkdir /usr/build
 RUN tar zxvf target/grib2json-0.8.0-SNAPSHOT.tar.gz -C /usr/build/
 RUN cd /usr/build/grib2json-0.8.0-SNAPSHOT/bin
-# RUN sh grib2json trololo  
+RUN chmod +x essai.sh
+# RUN sh grib2json trololo
---
+++ tracker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# message="Vous êtes dans le dossier"
+# cmd=`pwd`
+# read -p "Quel est votre prénom et votre age ? " prenom age
+# echo "Tu t'appelles $prenom et tu as $age."
+# testTableau=('yo' 'zyva' 'ouech' 'ouechouech')
+# echo ${testTableau[2]}
+# let "a = 2 ** 2"
+
+# echo $0 # affiche le nom du script
+# echo $# # affiche le nombre de param
+# echo $1 # affiche le param 1 ($2 affiche le param 2 etc...)
+
+echo $1
